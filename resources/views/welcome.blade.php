80383eb4313a57d5c67753e74f4aada6b848853b
Masudrana09871@@ <masudranamd112@gmail.com>
2022-12-08 11:23:04 -0800

    update 1:22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,9 @@
         </style>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js" integrity="sha512-CX7sDOp7UTAq+i1FYIlf9Uo27x4os+kGeoT7rgwvY+4dmjqV0IuE/Bl5hVsjnQPQiTOhAX1O2r2j5bjsFBvv/A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-        @vite(['resources\js\app.js'])
+        {{-- @vite(['resources\js\app.js']) --}}
         
+        <script src="{{asset('build\assets\app.507cbaa1.js')}}"></script>
 
     </head>
     <body class="antialiased">
